e1bef51ce6c2472b6b95a57d47a5d06c7e00e132
mochimochimoon <mochi.mochi.moon5098@gmail.com>
2018-06-07 22:42:34 +0900

    feat(tab):tab切り替えの動き・デザインを実装 #1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-// neko
+(function() {
+  $('.js-tab-trigger').on('click', function() {
+    const $trigger = $(this);
+    const number = $trigger.attr('data-tab-number');
+    const $wrapper = $trigger.closest('.js-tab-wrapper');
+    const targetSelector = `.js-tab-contents[data-tab-number="${number}"]`;
+    const $target = $wrapper.find(targetSelector);
+    if (!($trigger.hasClass('is-selected'))) {
+      $wrapper.find('.is-selected').removeClass('is-selected');
+      $trigger.addClass('is-selected');
+      $wrapper.find('.js-tab-contents.is-active').removeClass('is-active');
+      $target.addClass('is-active');
+    }
+    return false;
+  });
+})();
