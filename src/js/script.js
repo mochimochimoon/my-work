0069e76169a05dfd7679253b074fcc5488f2e2d9
mochimochimoon <mochi.mochi.moon5098@gmail.com>
2018-06-07 23:11:30 +0900

    feat(column-change):カラム変更の動きとデザインを実装 #1

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,32 @@
 (function() {
+  // Tab
   $('.js-tab-trigger').on('click', function() {
     const $trigger = $(this);
-    const number = $trigger.attr('data-tab-number');
-    const $wrapper = $trigger.closest('.js-tab-wrapper');
-    const targetSelector = `.js-tab-contents[data-tab-number="${number}"]`;
-    const $target = $wrapper.find(targetSelector);
     if (!($trigger.hasClass('is-selected'))) {
-      $wrapper.find('.is-selected').removeClass('is-selected');
+      const number = $trigger.attr('data-tab-number');
+      const $wrapper = $trigger.closest('.js-tab-wrapper');
+      const targetSelector = `.js-tab-contents[data-tab-number="${number}"]`;
+      const $target = $wrapper.find(targetSelector);
+      $wrapper.find('.js-tab-trigger.is-selected').removeClass('is-selected');
       $trigger.addClass('is-selected');
       $wrapper.find('.js-tab-contents.is-active').removeClass('is-active');
       $target.addClass('is-active');
+    }
+    return false;
+  });
+
+  // Column
+  $('.js-col-trigger').on('click', function() {
+    const $trigger = $(this);
+    if (!($trigger.hasClass('is-selected'))) {
+      const colClass = `is-${$trigger.attr('data-col')}-col`;
+      const $wrapper = $trigger.closest('.js-col-wrapper');
+      $wrapper.find('.js-col-trigger.is-selected').removeClass('is-selected');
+      $trigger.addClass('is-selected');
+      $wrapper.removeClass(function(index, className) {
+        return (className.match(/is-\d-col/g) || []).join(' '); // \dは任意の数字
+      });
+      $wrapper.addClass(colClass);
     }
     return false;
   });
